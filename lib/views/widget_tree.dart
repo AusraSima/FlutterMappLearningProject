65ebd5f867836a2164794a373af967dae33f0d1e
AusraSima <ausrsima@yahoo.com>
2026-08-09 19:22:45 +0300

    Initial commit

---
+++ lib/views/widget_tree.dart
@@ -0,0 +1,28 @@
+import 'package:book_application/data/notifiers.dart';
+import 'package:book_application/views/pages/home_oage.dart';
+import 'package:book_application/views/pages/profile_page.dart';
+import 'package:flutter/material.dart';
+import 'widgets/navbar_widget.dart';
+
+List<Widget> pages = [
+  HomePage(),
+  ProfilePage(),
+];
+
+class WidgetTree extends StatelessWidget {
+  const WidgetTree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Mapp'),
+        centerTitle: true, // Sucentravimui Andoiduose, iOS nebutinas
+      ),
+      body: ValueListenableBuilder(valueListenable: selectedPageNotifier, builder: (context, selectedPage, child) {
+        return pages.elementAt(selectedPage);
+      }),
+      bottomNavigationBar: NavbarWidget(),
+    );
+  }
+}
