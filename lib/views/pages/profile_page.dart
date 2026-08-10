b0346931ff658004e26e0cd0f6b2269932200b9f
AusraSima <ausrsima@yahoo.com>
2026-08-10 17:52:00 +0300

    Adding User Input

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -1,12 +1,83 @@
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  TextEditingController controller = TextEditingController();
+  bool? isChecked = false;
+  bool isSwitched = false;
+  double sliderValue = 0.0;
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Profile Page'),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(border: OutlineInputBorder()),
+              onEditingComplete: () {
+                setState(() {});
+              },
+            ),
+            Text(controller.text),
+            Checkbox.adaptive(
+              tristate: true,
+              value: isChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  isChecked = value;
+                });
+              },
+            ),
+            CheckboxListTile.adaptive(
+              tristate: true,
+              title: Text('Click me'),
+              value: isChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  isChecked = value;
+                });
+              },
+            ),
+            Switch.adaptive(
+              value: isSwitched,
+              onChanged: (bool value) {
+                setState(() {
+                  isSwitched = value;
+                });
+              },
+            ),
+            SwitchListTile.adaptive(
+              title: Text('Switch me'),
+              value: isSwitched,
+              onChanged: (bool value) {
+                setState(() {
+                  isSwitched = value;
+                });
+              },
+            ),
+            Slider.adaptive(
+              max: 10.0,
+              value: sliderValue,
+              divisions: 10,
+              onChanged: (double value) {
+                setState(() {
+                  sliderValue = value;
+                });
+                print(value);
+              },
+            ),
+            Image.asset('assets/images/bg.jpg'),
+          ],
+        ),
+      ),
     );
   }
 }
