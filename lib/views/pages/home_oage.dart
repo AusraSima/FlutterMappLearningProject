4be4923cb86e70d32a15694efd564c401d043027
AusraSima <ausrsima@yahoo.com>
2026-08-11 14:00:01 +0300

    Adding Divider, Cart and Text

--- lib/views/pages/home_oage.dart
+++ lib/views/pages/home_oage.dart
@@ -1,3 +1,4 @@
+import 'package:book_application/data/constants.dart';
 import 'package:book_application/views/widgets/hero_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -8,7 +9,30 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(20.0),
-      child: Column(children: [HeroWidget()]),
+      child: Column(
+        children: [
+          HeroWidget(),
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(vertical: 10.0),
+            child: Card(
+              child: Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Basic Layout', style: KTextStyle.titleTealText),
+                    Text(
+                      'The description of this',
+                      style: KTextStyle.descriptionText,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
