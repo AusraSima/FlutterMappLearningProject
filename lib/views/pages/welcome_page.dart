4aa713703225779d17c378f181c06b09d197ccc0
AusraSima <ausrsima@yahoo.com>
2026-08-11 12:26:22 +0300

    Adding Hero

--- lib/views/pages/welcome_page.dart
+++ lib/views/pages/welcome_page.dart
@@ -1,4 +1,5 @@
 import 'package:book_application/views/widget_tree.dart';
+import 'package:book_application/views/widgets/hero_widget.dart';
 import 'package:flutter/material.dart';
 
 class WelcomePage extends StatelessWidget {
@@ -12,10 +13,7 @@ class WelcomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20.0),
-              child: Image.asset('assets/images/bg.jpg'),
-            ),
+            HeroWidget(),
             FilledButton(onPressed: () {
               Navigator.pushReplacement(
                 context,
