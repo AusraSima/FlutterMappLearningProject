4094c50868dc7bdf6ae803ffb0396b478dc7272d
AusraSima <ausrsima@yahoo.com>
2026-08-12 21:15:03 +0300

    Adding LoginPage

--- lib/views/pages/welcome_page.dart
+++ lib/views/pages/welcome_page.dart
@@ -1,4 +1,4 @@
-import 'package:book_application/views/widget_tree.dart';
+import 'package:book_application/views/pages/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
@@ -31,7 +31,7 @@ class WelcomePage extends StatelessWidget {
                   context,
                   MaterialPageRoute(
                     builder: (context) {
-                      return WidgetTree();
+                      return LoginPage();
                     },
                   ),
                 );
@@ -47,7 +47,7 @@ class WelcomePage extends StatelessWidget {
                   context,
                   MaterialPageRoute(
                     builder: (context) {
-                      return WidgetTree();
+                      return LoginPage();
                     },
                   ),
                 );
